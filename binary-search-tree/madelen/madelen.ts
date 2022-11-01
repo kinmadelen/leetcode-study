87c81e4557b62e98abeb700a1b7d869fd44eab39
Madelen <rolemadelen@pm.me>
2022-11-01 22:50:31 +0900

    tree traversal and searching in bst

--- binary-search-tree/madelen/madelen.ts
+++ binary-search-tree/madelen/madelen.ts
@@ -31,13 +31,13 @@ export class BST<T> {
           if (curr.left) curr = curr.left;
           else {
             curr.left = new BinarySearchTreeNode<T>(value);
-            break;
+            return;
           }
         } else {
           if (curr.right) curr = curr.right;
           else {
             curr.right = new BinarySearchTreeNode<T>(value);
-            break;
+            return;
           }
         }
       }
@@ -62,11 +62,59 @@ export class BST<T> {
     return node;
   }
 
+  search(value: T): boolean {
+    if (null === this.root) return false;
+
+    let curr: BSTNode<T> = this.root;
+
+    while (curr) {
+      if (value === curr.value) {
+        return true;
+      } else if (value < curr.value) {
+        curr = curr.left;
+      } else {
+        curr = curr.right;
+      }
+    }
+
+    return false;
+  }
+
+  searchRecur(value: T, node: BSTNode<T> = this.root): boolean {
+    if (!node) {
+      return false;
+    } else {
+      if (value === node.value) return true;
+
+      if (value < node.value) {
+        return this.searchRecur(value, node.left);
+      } else {
+        return this.searchRecur(value, node.right);
+      }
+    }
+  }
+
+  preorder(node: BSTNode<T> = this.root) {
+    if (null === node) return;
+    process.stdout.write(`${node?.value} `);
+    this.preorder(node?.left);
+    this.preorder(node?.right);
+  }
+
   inorder(node: BSTNode<T> = this.root) {
     if (null === node) return;
+
     this.inorder(node?.left);
-    console.log(node?.value);
+    process.stdout.write(`${node?.value} `);
     this.inorder(node?.right);
+  }
+
+  postorder(node: BSTNode<T> = this.root) {
+    if (null === node) return;
+
+    this.postorder(node?.left);
+    this.postorder(node?.right);
+    process.stdout.write(`${node?.value} `);
   }
 }
 
@@ -84,4 +132,20 @@ bst.insert(30);
 bst.insert(35);
 bst.insert(32);
 bst.insert(33);
+console.log('Preorder:');
+bst.preorder();
+console.log();
+
+console.log('Inorder:');
 bst.inorder();
+console.log();
+
+console.log('Postorder:');
+bst.postorder();
+console.log();
+
+console.log(`20 in the tree? ${bst.searchRecur(20)}`);
+console.log(`7 in the tree? ${bst.searchRecur(7)}`);
+console.log(`15 in the tree? ${bst.searchRecur(15)}`);
+console.log(`35 in the tree? ${bst.searchRecur(35)}`);
+console.log(`-3 in the tree? ${bst.searchRecur(-3)}`);
